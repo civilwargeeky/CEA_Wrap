5ee2ac0d01c48cf66d7158e1cac5bae949ec53bb
Daniel Klinger <civilwargeeky@yahoo.com>
2021-12-23 18:06:53 -0600

    Created ui basis for gui and added json of all elements that we can use

---
+++ CEA_Wrap/gui.py
@@ -0,0 +1,5 @@
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QFileDialog, QMessageBox, QMenu, QAction
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor, QCursor
+import pyqtgraph as pg

cf27fdaa5fe14af13896b3af60699dee85128612
Daniel Klinger <civilwargeeky@yahoo.com>
2026-04-17 13:59:57 -0700

    Make thermo lib helpers accessible under root namespace

--- CEA_Wrap/__init__.py
+++ CEA_Wrap/__init__.py
@@ -1,6 +1,7 @@
 import os as _os
 from .wrapper import *
 from .utils import *
+from .thermo_lib import *
 
 with open(_os.path.join(_os.path.dirname(__file__), "version.txt"), "r") as f:
     __version__ = f.read().strip()
